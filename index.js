be578e7cdbfb3ef1338c67b579269cc7b5bf5de1
abrasasha <abra_sasha228@mail.ru>
2022-12-27 09:48:01 +0000

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-import express from "express"
-import bodyParser from "body-parser"
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) =>
-{
-
-    res.render ("index.ejs");
-})
-app.post('/', (req,res) =>
-{
-    const username = req.body.username;
-    res.render('hello.ejs', {username: username})
-})
+import express from 'express'
+import bodyParser from 'body-parser'
+const app = express()
 const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`listen port: ${port}`))
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+      res.render('index.ejs')
+});
+
+app.post('/', (req, res) =>
+    {
+        const username = req.body.username;
+        res.render('hello.ejs', {username: username});
+    });
+
+app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+})
